92887ec76a8d26bd42f6cbf0e5f33b2f6bb230a3
Kurt Griffiths <kurt.griffiths@rackspace.com>
2011-01-11 21:41:26 +0000

    Implements #739

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `pollcat`
+CREATE DATABASE IF NOT EXISTS `rse`
   CHARACTER SET = utf8;
   
-USE pollcat;
+USE rse;
   
 CREATE TABLE IF NOT EXISTS `Events` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -21,13 +21,13 @@ CREATE TABLE IF NOT EXISTS `Events` (
 DROP EVENT IF EXISTS `evt_GarbageCollection`;
 CREATE EVENT `evt_GarbageCollection` ON SCHEDULE EVERY 1 MINUTE STARTS '2010-11-08 14:57:32' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM Events WHERE Events.created_at < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 2 MINUTE);
 
-create user 'pollcat'@'%' identified by 'insert-password-here';
+create user 'rse'@'%' identified by 'insert-password-here';
 
 -- Only for slave
-grant select, execute on pollcat.* to 'pollcat'@'%';
+grant select, execute on rse.* to 'rse'@'%';
 
 -- Only for master
-grant insert on pollcat.* to 'pollcat'@'%';
+grant insert on rse.* to 'rse'@'%';
 create user 'servant'@'%' identified by  'insert-password-here';
 grant REPLICATION SLAVE ON *.* to 'servant'@'%';
 
