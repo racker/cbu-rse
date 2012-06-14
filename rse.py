f081f40f90f9da56edc899b58f23a459779f7488
kgriffs <sffirgk@gmail.com>
2012-06-14 13:27:09 -0400

    Fixed bugs relating to optional arguments

--- rse.py
+++ rse.py
@@ -41,10 +41,11 @@ from controllers.shared import *
 from controllers.health_controller import *
 from controllers.main_controller import *
 
+
 class RseApplication(rawr.Rawr):
   """RSE app for encapsulating initialization"""
-  
-  def __init__(self): 
+
+  def __init__(self):
     rawr.Rawr.__init__(self)
 
     # Initialize config paths
@@ -52,11 +53,19 @@ class RseApplication(rawr.Rawr):
     local_config_path = os.path.join(dir_path, 'rse.conf')
     global_config_path = '/etc/rse.conf'
     default_config_path = os.path.join(dir_path, 'rse.default.conf')
-    
+
     # Parse options
-    config = ConfigParser.ConfigParser()
+    config = ConfigParser.ConfigParser(
+      defaults = {
+        'timeout': '5',
+        'authtoken_retention_period': '30',
+        'authtoken_slice_size': '2',
+        'replica-set': '[none]'
+      }
+    )
+
     config.read(default_config_path)
-    
+
     if os.path.exists(local_config_path):
        config.read(local_config_path)
     elif os.path.exists(global_config_path):
@@ -70,34 +79,27 @@ class RseApplication(rawr.Rawr):
     formatter = logging.Formatter('%(asctime)s - RSE - PID %(process)d - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s')
 
     if config.getboolean('logging', 'filelog'):
-      handler = logging.handlers.RotatingFileHandler(config.get('logging', 'filelog-path'), maxBytes=5 * 1024*1024, backupCount=5)    
+      handler = logging.handlers.RotatingFileHandler(config.get('logging', 'filelog-path'), maxBytes=5 * 1024*1024, backupCount=5)
       handler.setFormatter(formatter);
       logger.addHandler(handler)
-    
+
     if config.getboolean('logging', 'syslog'):
-      handler = logging.handlers.SysLogHandler(address=config.get('logging', 'syslog-address'))    
+      handler = logging.handlers.SysLogHandler(address=config.get('logging', 'syslog-address'))
       handler.setFormatter(formatter);
       logger.addHandler(handler)
-    
+
     # FastCache for Auth Token
     retention_period = config.getint('fastcache', 'authtoken_retention_period')
     slice_size = config.getint('fastcache', 'authtoken_slice_size')
-
-    if not retention_period: 
-      retention_period = 30
-
-    if not slice_size: 
-      slice_size = 2
-
     authtoken_cache = FastCache(retention_period, slice_size)
-  
+
     # Master instance connection for the health checker
     connection_master = pymongo.Connection(config.get('mongodb', 'uri'), read_preference=pymongo.ReadPreference.PRIMARY)
     mongo_db_master = connection_master[config.get('mongodb', 'database')]
 
     # General connection for regular requests
     # Note: Use one global connection to the DB across all handlers (pymongo manages its own connection pool)
-    replica_set = config.get('mongodb', 'replica-set') if config.has_option('mongodb', 'replica-set') else '[none]'
+    replica_set = config.get('mongodb', 'replica-set')
     if replica_set == '[none]':
       connection = pymongo.Connection(config.get('mongodb', 'uri'), read_preference=pymongo.ReadPreference.SECONDARY)
     else:
@@ -106,10 +108,10 @@ class RseApplication(rawr.Rawr):
       except Exception as ex:
         logger.warning( "Mongo connection exception: %s" % (ex.message))
         if ex.message == 'secondary':
-          return 
+          return
 
     mongo_db = connection[config.get('mongodb', 'database')]
-    
+
     # Initialize collections
     for i in range(10):
       try:
@@ -122,29 +124,26 @@ class RseApplication(rawr.Rawr):
     # WARNING: Counter must start at a value greater than 0 per the RSE spec!
     if not mongo_db.counters.find_one({'_id': 'last_known_id'}):
       mongo_db.counters.insert({'_id': 'last_known_id', 'c': 0})
-    
+
     accountsvc_host = config.get('account-services', 'host')
     accountsvc_https = config.getboolean('account-services', 'https')
-    accountsvc_timeout = config.getboolean('account-services', 'timeout')
+    accountsvc_timeout = config.getint('account-services', 'timeout')
     test_mode = config.getboolean('rse', 'test')
-  
-    if not accountsvc_timeout:
-      accountsvc_timeout = 5
-      
+
     # Setup routes
     shared = Shared(logger, authtoken_cache)
 
-    health_options = dict(shared=shared, accountsvc_host=accountsvc_host, 
-                          accountsvc_https=accountsvc_https, 
-                          accountsvc_timeout=accountsvc_timeout, 
+    health_options = dict(shared=shared, accountsvc_host=accountsvc_host,
+                          accountsvc_https=accountsvc_https,
+                          accountsvc_timeout=accountsvc_timeout,
                           mongo_db=mongo_db_master, test_mode=test_mode)
     self.add_route(r"/health$", HealthController, health_options)
 
-    main_options = dict(shared=shared, accountsvc_host=accountsvc_host, 
-                        accountsvc_https=accountsvc_https, 
+    main_options = dict(shared=shared, accountsvc_host=accountsvc_host,
+                        accountsvc_https=accountsvc_https,
                         accountsvc_timeout=accountsvc_timeout,
                         mongo_db=mongo_db, test_mode=test_mode)
-    self.add_route(r"/.+", MainController, main_options)    
+    self.add_route(r"/.+", MainController, main_options)
 
 # WSGI app
 app = RseApplication()
