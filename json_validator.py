f60277e2113a040a15fd96bd0e8432f24d07be40
Josh Brand <josh.brand@rackspace.com>
2013-11-12 14:03:55 -0500

    Revert "Merge branch 'pep8'"
    This reverts commit 1c96649232a7857fcab32750034fa01d5ee90b8e, reversing
    changes made to 19c42719839c24280ef28fafaacab77b25ffc283.

--- json_validator.py
+++ json_validator.py
@@ -54,29 +54,26 @@ ARRAYEND = 4
 COLON = 5
 COMMA = 6
 STRVALUE = 7
-VALUE = 8  # true, false, null, number
+VALUE = 8 # true, false, null, number
 
 # Transformations
 TRANSFORMATIONS = {}
-
-
 def _add_rule(startState, characters, endState, token):
-    """Add a rule to the transformations map.
+  """Add a rule to the transformations map.
 
-    Args:
-      startState: This rule only applies if the parser is in this state.
-      characters: This rule only applies if the current character is one of
-                  these.
-      endState: When applied, this rule changes the state this.
-      token: When applied, this rule adds this token to the stack.
-    """
-    # None is treated as a wildcard character.
-    if characters == None:
-        TRANSFORMATIONS[(startState, None)] = (endState, token)
-    else:
-        # Create a rule for every character.
-        for char in characters:
-            TRANSFORMATIONS[(startState, char)] = (endState, token)
+  Args:
+    startState: This rule only applies if the parser is in this state.
+    characters: This rule only applies if the current character is one of these.
+    endState: When applied, this rule changes the state this.
+    token: When applied, this rule adds this token to the stack.
+  """
+  # None is treated as a wildcard character.
+  if characters == None:
+    TRANSFORMATIONS[(startState, None)] = (endState, token)
+  else:
+    # Create a rule for every character.
+    for char in characters:
+      TRANSFORMATIONS[(startState, char)] = (endState, token)
 
 _add_rule(GROUND, " \r\n", GROUND, None)
 _add_rule(GROUND, "[", GROUND, ARRAYSTART)
@@ -150,111 +147,110 @@ _add_rule(ZERO, "}", GROUND, OBJECTEND)
 # List of states which are acceptable to end in.
 EXITSTATES = (GROUND, NUMBER, ZERO, EXPONENT3)
 
-
 def is_valid(string):
-    """Returns true if the string is valid syntax for a JSON array or object.
+  """Returns true if the string is valid syntax for a JSON array or object.
 
-    Args:
-      string: JSON string to check.
+  Args:
+    string: JSON string to check.
 
-    Returns:
-      True iff JSON string is valid.
-    """
-    state = GROUND
-    tokens = []
-    for char in string:
-        # Transform from this state to the next state.
-        next = TRANSFORMATIONS.get((state, char))
-        if next == None:
-            # No matching character, check for a wildcard match.
-            next = TRANSFORMATIONS.get((state, None))
-            if next == None:
-                return False
-        (state, token) = next
-        if token != None:
-            tokens.append(token)
-    if not state in EXITSTATES:
-        # A half-defined value.
+  Returns:
+    True iff JSON string is valid.
+  """
+  state = GROUND
+  tokens = []
+  for char in string:
+    # Transform from this state to the next state.
+    next = TRANSFORMATIONS.get((state, char))
+    if next == None:
+      # No matching character, check for a wildcard match.
+      next = TRANSFORMATIONS.get((state, None))
+      if next == None:
         return False
-    if not tokens or (tokens[0] != ARRAYSTART and tokens[0] != OBJECTSTART):
-        # Root value must be array or object.
-        return False
-    if not _pop_value(tokens):
-        # Not a value.
-        return False
-    if tokens:
-        # Leftover tokens beyond first value.
-        return False
-    return True
-
+    (state, token) = next
+    if token != None:
+      tokens.append(token)
+  if not state in EXITSTATES:
+    # A half-defined value.
+    return False
+  if not tokens or (tokens[0] != ARRAYSTART and tokens[0] != OBJECTSTART):
+    # Root value must be array or object.
+    return False
+  if not _pop_value(tokens):
+    # Not a value.
+    return False
+  if tokens:
+    # Leftover tokens beyond first value.
+    return False
+  return True
 
 def _pop_value(tokens):
-    """Do the provided JSON tokens form a value?  Starting from the end, pop
-    tokens off the list as they are used.  Unused tokens remain on the list.
-    This function is recursive.
+  """Do the provided JSON tokens form a value?  Starting from the end, pop
+  tokens off the list as they are used.  Unused tokens remain on the list.
+  This function is recursive.
 
-    Args:
-      tokens: List of JSON tokens.
+  Args:
+    tokens: List of JSON tokens.
 
-    Returns:
-      True iff JSON value is found.
-    """
-    if not tokens:
-        # Empty
-        return False
-    # Work backwards since t.pop() is much more efficent than del t[0].
-    token = tokens.pop()
-
-    if token == VALUE or token == STRVALUE:
-        return True
-
-    if token == ARRAYEND:
-        has_value = False
-        while tokens:
-            if tokens[-1] == ARRAYSTART:
-                tokens.pop()
-                return True
-            if has_value:
-                if tokens[-1] != COMMA:
-                    # Values not comma separated.
-                    return False
-                tokens.pop()
-            if not _pop_value(tokens):
-                # Array contains non-value.
-                return False
-            has_value = True
-        # Ran out of tokens looking for "["
-        return False
-
-    if token == OBJECTEND:
-        has_value = False
-        while tokens:
-            if tokens[-1] == OBJECTSTART:
-                tokens.pop()
-                return True
-            if has_value:
-                if tokens[-1] != COMMA:
-                    # Pairs not comma separated.
-                    return False
-                tokens.pop()
-            if not _pop_value(tokens):
-                # Object contains non-value.
-                return False
-            has_value = True
-            if not tokens:
-                break
-            if tokens[-1] != COLON:
-                # Name:value not colon separated.
-                return False
-            tokens.pop()
-            if not tokens:
-                break
-            if tokens[-1] != STRVALUE:
-                # Object property not a string.
-                return False
-            tokens.pop()
-        # Ran out of tokens looking for "{"
-        return False
-
-    # Must be a comma or colon.
+  Returns:
+    True iff JSON value is found.
+  """
+  if not tokens:
+    # Empty
     return False
+  # Work backwards since t.pop() is much more efficent than del t[0].
+  token = tokens.pop()
+
+  if token == VALUE or token == STRVALUE:
+    return True
+
+  if token == ARRAYEND:
+    has_value = False
+    while tokens:
+      if tokens[-1] == ARRAYSTART:
+        tokens.pop()
+        return True
+      if has_value:
+        if tokens[-1] != COMMA:
+          # Values not comma separated.
+          return False
+        tokens.pop()
+      if not _pop_value(tokens):
+        # Array contains non-value.
+        return False
+      has_value = True
+    # Ran out of tokens looking for "["
+    return False
+
+  if token == OBJECTEND:
+    has_value = False
+    while tokens:
+      if tokens[-1] == OBJECTSTART:
+        tokens.pop()
+        return True
+      if has_value:
+        if tokens[-1] != COMMA:
+          # Pairs not comma separated.
+          return False
+        tokens.pop()
+      if not _pop_value(tokens):
+        # Object contains non-value.
+        return False
+      has_value = True
+      if not tokens:
+        break
+      if tokens[-1] != COLON:
+        # Name:value not colon separated.
+        return False
+      tokens.pop()
+      if not tokens:
+        break
+      if tokens[-1] != STRVALUE:
+        # Object property not a string.
+        return False
+      tokens.pop()
+    # Ran out of tokens looking for "{"
+    return False
+
+  # Must be a comma or colon.
+  return False
+
