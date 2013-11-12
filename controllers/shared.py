c3c9a43d932dbe89c37d2032ebf35b1c898d17bd
Josh Brand <josh.brand@rackspace.com>
2013-11-12 12:15:43 -0500

    chore(shared): pep8

--- controllers/shared.py
+++ controllers/shared.py
@@ -3,25 +3,26 @@
 @author Kurt Griffiths
 
 @brief
-Defines the Shared class, which encapsulates performance counters and hard-coded options
+Defines the Shared class, which encapsulates performance counters and
+hard-coded options
 """
 
 import re
 
+
 class Shared:
-  """Encapsulates performance counters and hard-coded options"""
+    """Encapsulates performance counters and hard-coded options"""
+    def __init__(self, logger, authtoken_cache):
+        self.authtoken_cache = authtoken_cache
+        self.logger = logger
 
-  def __init__(self, logger, authtoken_cache):
-    self.authtoken_cache = authtoken_cache
-    self.logger = logger
+        self.cache_token_hitcnt = 0
+        self.cache_token_totalcnt = 0
+        self.id_totalcnt = 0
+        self.id_retrycnt = 0
 
-    self.cache_token_hitcnt = 0
-    self.cache_token_totalcnt = 0
-    self.id_totalcnt = 0;
-    self.id_retrycnt = 0;
- 
-    self.AUTH_ENDPOINT = '/v1.0/auth/isauthenticated'
-    self.AUTH_HEALTH_ENDPOINT = '/v1.0/help/apihealth'
+        self.AUTH_ENDPOINT = '/v1.0/auth/isauthenticated'
+        self.AUTH_HEALTH_ENDPOINT = '/v1.0/help/apihealth'
 
-    # Precompiled regex for validating JSONP callback name
-    self.JSONP_CALLBACK_PATTERN = re.compile("\A[a-zA-Z0-9_]+\Z") 
+        # Precompiled regex for validating JSONP callback name
+        self.JSONP_CALLBACK_PATTERN = re.compile("\A[a-zA-Z0-9_]+\Z")
