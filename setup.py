3493e51496fc56e04d2670542edcf79a3133deb7
Andrew Vant <andrew.vant@rackspace.com>
2017-11-30 18:49:11 -0500

    Update setup.py files to account for moved packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="rse",
     version="2.1.0",
+    packages = find_packages(),
     description="Real Simple Events",
     url="https://github.com/rackerlabs/rse/",
     maintainer="ATL Devops",
     maintainer_email="devops.atl@lists.rackspace.com",
     classifiers=["Private :: Do Not Upload"],
-    install_requires=["rax", "eom>=0.8.0", "pymongo<3"],
-    packages=["controllers"],
+    install_requires=["eom>=0.8.0", "pymongo<3", "webob"],
     py_modules=["rse", "json_validator", "rseutils"]
 )
