a1aa7ffbba86b5efe8009f1a1d639ff7a130a483
Andrew Vant <andrew.vant@rackspace.com>
2018-03-07 17:33:21 -0500

    Allow cli-specified conf files

--- src/rse/cmd.py
+++ src/rse/cmd.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python2
 
+import sys
+
 from wsgiref.simple_server import make_server
 
 import rse
 
 # If running rse directly, startup a basic WSGI server for testing
 def main():
-    app = rse.instantiate()
+    conf = None
+    if len(sys.argv) > 1:
+        conf = sys.argv[1]
 
+    app = rse.RseApplication(conf)
     httpd = make_server('', 8000, app)
     print "Serving on port 8000..."
     httpd.serve_forever()
