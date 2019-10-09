8f4dfa7656e313e35dc2ce2075cbb0a01978d131
Benjamen Meyer <bm_witness@yahoo.com>
2019-10-09 17:47:16 -0400

    Add more unit tests
    Taken from an old commit of Ben's, 9b6730cd.

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,4 +1,18 @@
 import unittest
+
+
 class TestImports(unittest.TestCase):
     def test_import_rse(self):
         import rse
+
+    def test_import_controllers(self):
+        import rse.controllers
+
+    def test_import_rax(self):
+        import rse.rax
+
+    @unittest.expectedFailure
+    def test_import_wsgi_app(self):
+        # configuration needs to be fixed before
+        # this will pass properly
+        from rse.wsgi import app
