9825a9d698fcaee2af5f26c940a61be490b882de
Andrew Vant <ajvant@gmail.com>
2020-02-13 10:45:41 -0500

    Fix pep8 issues in http/exceptions.py

--- src/rse/rax/http/exceptions.py
+++ src/rse/rax/http/exceptions.py
@@ -6,7 +6,8 @@ $Revision: 835 $  <=== populated-by-subversion
 $Date: 2011-01-10 14:15:28 -0500 (Mon, 10 Jan 2011) $  <=== populated-by-subversion
 
 @brief
-HTTP exceptions (see also http://en.wikipedia.org/wiki/List_of_HTTP_status_codes)
+HTTP exceptions (see also
+http://en.wikipedia.org/wiki/List_of_HTTP_status_codes)
 
 @pre
 Requires Python 2.x (tested with 2.7)
@@ -17,13 +18,14 @@ Convert class comments to python-style
 
 import http.client
 
+
 # Represents a generic HTTP error status. Raise this or one of the
 # predefined child classes to return a status other than "200 OK"
 # from a Rawr controller
 class HttpError(Exception):
     __slots__ = ['status_code', 'info']
 
-    def __init__(self, status_code, info = ''):
+    def __init__(self, status_code, info=''):
         self.status_code = status_code
         self.info = info
 
@@ -31,27 +33,33 @@ class HttpError(Exception):
         return "HttpError: %d (%s)" % (self.status_code, self.info)
 
     def status(self):
-        return '%d %s' % (self.status_code, http.client.responses[self.status_code])
+        msg = http.client.responses[self.status_code]
+        return '%d %s' % (self.status_code, msg)
+
 
 # 204 No Content
 class HttpNoContent(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 204, info)
+
 
 # 201 Created
 class HttpCreated(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 201, info)
+
 
 # 202 Accepted
 class HttpAccepted(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 202, info)
+
 
 # 400 Bad Request
 class HttpBadRequest(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 400, info)
+
 
 # 401 Unauthorized
 class HttpUnauthorized(HttpError):
@@ -59,47 +67,57 @@ class HttpUnauthorized(HttpError):
     def __init__(self):
         HttpError.__init__(self, 401)
 
+
 # 403 Forbidden
 class HttpForbidden(HttpError):
     # Don't return a reason (security best practice)
     def __init__(self):
         HttpError.__init__(self, 403)
 
+
 # 404 Not Found
 class HttpNotFound(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 404, info)
+
 
 # 405 Method Not Allowed
 class HttpMethodNotAllowed(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 405, info)
+
 
 # 412 Precondition Failed
 class HttpPreconditionFailed(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 412, info)
+
 
 # 415 Unsupported Media Type
 class HttpUnsupportedMediaType(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 415, info)
+
 
 # 409 Conflict
 class HttpConflict(HttpError):
-    def __init__(self, info = ''):
+    def __init__(self, info=''):
         HttpError.__init__(self, 409, info)
+
 
 # 500 Internal Server Error
 class HttpInternalServerError(HttpError):
     def __init__(self):
         HttpError.__init__(self, 500)
 
+
 # 502 Bad Gateway
-# The server was acting as a gateway or proxy and received an invalid response from the upstream server.
+# The server was acting as a gateway or proxy and received an invalid response
+# from the upstream server.
 class HttpBadGateway(HttpError):
     def __init__(self):
         HttpError.__init__(self, 502)
+
 
 # 503 Service Unavailable
 class HttpServiceUnavailable(HttpError):
